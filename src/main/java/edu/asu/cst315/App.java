aa058ecfc6cf6626c57b4423abdd2d3dbee84971
Douglas Greenbaum <dggreenbaum@greenbad.org>
2015-02-04 20:15:33 -0700

    Added a play state

--- src/main/java/edu/asu/cst315/App.java
+++ src/main/java/edu/asu/cst315/App.java
@@ -6,16 +6,20 @@ import org.newdawn.slick.state.StateBasedGame;
 public class App extends StateBasedGame {
 
     public static final int menu = 0;
+    public static final int play = 1;
 
     public App(String title) {
         super(title);
         this.addState(new Menu(menu));
+        this.addState(new Play(play));
     }
 
 
     @Override
     public void initStatesList(GameContainer gameContainer) throws SlickException {
         this.getState(menu).init(gameContainer, this);
+        this.getState(play).init(gameContainer, this);
+        this.enterState(menu);
     }
 
     public static void main(String[] args) throws SlickException {
---
+++ src/main/java/edu/asu/cst315/Play.java
@@ -0,0 +1,39 @@
+package edu.asu.cst315;
+
+import org.lwjgl.input.Mouse;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+import java.io.File;
+
+public class Play extends BasicGameState {
+
+    String image_path = System.getProperty("user.dir") + File.separator + "src" + File.separator + "main" + File.separator + "java" + File.separator + "edu" + File.separator + "asu" + File.separator + "cst315" + File.separator + "img";
+
+    public Play(int state) {
+    }
+
+    @Override
+    public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int delta) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
+
+    }
+
+    @Override
+    public int getID() {
+        return 1;
+    }
+}
